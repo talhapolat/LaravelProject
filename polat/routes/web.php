ab42b3cdf486aa4e4c8fe164bde625b91bee51e8
talhapolat <talhapolat@hotmail.com>
2020-05-07 02:38:54 +0300

    Update

--- polat/routes/web.php
+++ polat/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/urun/{isim}/{soyad?}', function ($isim, $soyad=null) {
+    return "Merhaba: $isim $soyad";
 });
